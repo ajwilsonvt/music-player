b683bd86c21ca92ed0bd344369b63483c15b58a2
ajwilsonvt <ajwilsonvt@users.noreply.github.com>
2017-10-19 11:37:25 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,12 +2,22 @@ import list from './LinkedList.js';
 // var list exists
 // https://stackoverflow.com/questions/43551210/how-do-i-pass-constructor-parameters-to-imported-class-in-ecmascript-6
 
+var audios = null;
+var previousSong = 0;
+var currentSong = 0;
+
 $(function() {
   
+  /**
+   * Search for song
+   */
   $('#search').on('submit', function(e) {
     e.preventDefault();
-    $('#add').empty();
 
+    $('#add').empty();
+    audios = null;
+    previousSong = 0;
+    currentSong = 0;
     var query = $('#query').val();
     console.log('queried', query);
 
@@ -24,6 +34,7 @@ $(function() {
       console.log(res);
 
       $('#add').append(`
+        <h3>Playlist</h3>
         <input type="submit" value="ADD TO PLAYLIST">
       `);
       var results = res.results;
@@ -49,6 +60,9 @@ $(function() {
     });
   });
 
+  /**
+   * Add to playlist
+   */
   $('#add').on('submit', function(e) {
     e.preventDefault();
 
@@ -56,14 +70,72 @@ $(function() {
     var songs = jQuery.makeArray($songs);
     console.log(songs);
     songs.forEach(function(el) {
-      list.append(el);
+      list.prepend(el);
     });
 
     $('#add').empty();
+    $('#add').append(`
+        <h3>Playlist</h3>
+      `);
     for (let i = 0; i < list.size; i++) {
       $('#add').append(list.get(i));
     }
     $('input:checked').css('visibility','hidden');
+
+    console.log(list);
   });
+
+  /**
+   * Music player controls
+   */
+  
+  // play button
+  $('#play').on('click', function() {
+    if (audios) {
+      audios[currentSong].play();
+    } else {
+      playFirst();
+    }
+  });
+
+  // previous button
+  $('#previous').on('click', function() {
+    if (currentSong === 0) return;
+    changeSong(-1);
+  });
+
+  // next button
+  $('#next').on('click', function() {
+    if (audios && currentSong === audios.length - 1) return;
+    changeSong(1);
+  });
+
+  // stop button
+  $('#stop').on('click', function() {
+    audios[currentSong].pause();
+    audios[currentSong].currentTime = 0;
+  });
+
+  function playFirst() {
+    var $audios = $('audio');
+    audios = $audios;
+    audios[0].play();
+    currentSong = 0;
+  }
+
+  function changeSong(val) {
+    previousSong = currentSong;
+    currentSong += val;
+    console.log('previous', previousSong);
+    console.log('current', currentSong);
+
+    if (audios) {
+      audios[previousSong].pause();
+      audios[previousSong].currentTime = 0;
+      audios[currentSong].play();
+    } else {
+      playFirst();
+    }
+  }
 
 });
